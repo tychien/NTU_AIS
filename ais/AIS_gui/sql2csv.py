467658b0ec7db50ba0ceb01860a820acd92e4dd1
tychien <a9114242@icloud.com>
2022-09-27 23:19:40 +0800

    ADD SQL2CSV.PY by tychien

--- ais/AIS_gui/sql2csv.py
+++ ais/AIS_gui/sql2csv.py
@@ -1,21 +1,49 @@
+from asyncore import read
+import os 
 from datetime import datetime
 from csv import DictReader, DictWriter, Sniffer
 
 def sqlToCsv(ORIGFILE,NEWFILE):
     #check if column exist 
-    with open(ORIGFILE,'r') as read_obj:
+    HAS_HEADER = False
+    with open(ORIGFILE,'r') as read_obj, open(NEWFILE, 'w') as write_obj:
         sniffer = Sniffer()
-        has_header = sniffer.has_header(read_obj.read(13333))
+        has_header = sniffer.has_header(read_obj.read(333333333333))
         read_obj.seek(0)
-        dict_r = DictReader(read_obj)
+        dict_r = DictReader(read_obj,delimiter="\t")
         headers= dict_r.fieldnames
         print(headers)
         
         if has_header: 
-            print("HAS HEADER")
-            print()
+            HAS_HEADER = True
+            #print("HAS HEADER")
         else:
-            print("no header")
+            HAS_HEADER = False
+            #print("no header")
+        
+        print("Has header") if HAS_HEADER else print("No header")
+        
+        if HAS_HEADER is False:
+            #with open(NEWFILE, 'a') as write_csv:
+            #fieldnamesHeaders = {'IMO_NUMBER','Call_Sign','ShipName','MMSI','Navigational_Status','ROT','SOG','Position_Accuracy','Longitude','Latitude','COG','True_Heading','Time_Stamp','Communication_Status','Ship_and_Cargo_Type','Reference_Position_A','Reference_Position_B','Reference_Position_C','Reference_Position_D','Fixing_Device','ETA','MAX_Draught','Destination','DTE','Gross_Tonnage'}
+            
+            #writer = DictWriter(write_obj,fieldnames=fieldnamesHeaders, delimiter='\t')
+            #writer.writeheader()
+            read_obj.seek(0)
+            content = read_obj.read()
+            
+            write_obj.write("IMO_NUMBER\tCall_Sign\tShipName\tMMSI\tNavigational_Status\tROT\tSOG\tPosition_Accuracy\tLongitude\tLatitude\tCOG\tTrue_Heading\tTime_Stamp\tCommunication_Status\tShip_and_Cargo_Type\tReference_Position_A\tReference_Position_B\tReference_Position_C\tReference_Position_D\tFixing_Device\tETA\tMAX_Draught\tDestination\tDTE\tGross_Tonnage\n"+content)
+            #write_obj.write(content)
+            HAS_HEADER = True
+
+        print("shit")
+
+    with open(NEWFILE, 'r') as read_obj:
+        dict_r = DictReader(read_obj, delimiter='\t')
+        for row in dict_r:
+            long = (row['Latitude'])
+            print(long)
+
 
 
 
@@ -24,10 +52,10 @@ def sqlToCsv(ORIGFILE,NEWFILE):
 
 
 
-
-
 if __name__ == '__main__':
-    orig_path = '/Users/tychien/NTU_AIS/ais/AIS_gui/ais_201306_2.sql'
+    HOME = os.path.expanduser( '~' )
+
+    orig_path = HOME+'/NTU_AIS/ais/AIS_gui/ais_201306_1.sql'
     #orig_path = '/Users/tychien/NTU_AIS/ais/AIS_gui/CHe1_5K.csv'
     new_path  = 'ais_new_1.csv'
     sqltest = sqlToCsv(orig_path, new_path)
